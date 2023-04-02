f5832c86f7732214d5835537f9315dc20d12ee2e
email <allentusar909@gmail.com>
2023-04-03 00:04:04 +0600

    learning vary important topics on this module

--- src/components/PhoneBar/PhoneBar.jsx
+++ src/components/PhoneBar/PhoneBar.jsx
@@ -15,37 +15,38 @@ import axios from "axios";
 const PhoneBar = () => {
   const [phones, setPhones] = useState([]);
 
-  /* useEffect(() => {
-    fetch("https://openapi.programming-hero.com/api/phones?search=iphone")
-      .then((res) => res.json())
-      .then((data) => setPhones(data));
-  }, []); */
-
-  axios
-    .get("https://openapi.programming-hero.com/api/phones?search=iphone")
-    .then((data) => {
-      const loadedData = data.data.data;
-      console.log(loadedData);
-      const phonesData = loadedData.map((phone) => {
-        const parts = phone.slug.split("-");
-        const price = parseInt(parts[1]);
-        const phoneInfo = {
-          name: phone.phone_name,
-          price: price,
-        };
-        return phoneInfo;
+  useEffect(() => {
+    // fetch("https://openapi.programming-hero.com/api/phones?search=iphone")
+    //   .then((res) => res.json())
+    //   .then((data) => setPhones(data));
+    axios
+      .get("https://openapi.programming-hero.com/api/phones?search=iphone")
+      .then((data) => {
+        const loadedData = data.data.data;
+        //   console.log(loadedData);
+        const phonesData = loadedData.map((phone) => {
+          const parts = phone.slug.split("-");
+          console.log(parts);
+          const price = parseInt(parts[1]);
+          const phoneInfo = {
+            name: phone.phone_name,
+            price: price,
+          };
+          return phoneInfo;
+        });
+        //   console.log(phonesData);
+        setPhones(phonesData);
       });
-      console.log(phonesData);
-      setPhones(phonesData);
-    });
+  }, []);
 
+  console.log(phones);
   return (
     <div>
       <BarChart width={1000} height={400} data={phones}>
-      <Bar dataKey="price" fill="#8884d8" />
-      <XAxis dataKey="name"></XAxis>
-      <YAxis></YAxis>
-      <Tooltip></Tooltip>
+        <Bar dataKey="price" fill="#8884d8" />
+        <XAxis dataKey="name"></XAxis>
+        <YAxis></YAxis>
+        <Tooltip></Tooltip>
       </BarChart>
     </div>
   );
